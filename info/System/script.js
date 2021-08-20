d3a6692055981699d25619cd69e2f61f2230d8a1
System Administrator <nguyendinhkhanh06@gmail.com>
2021-08-20 16:51:03 +0700

    Initial commit

---
+++ info/System/script.js
@@ -0,0 +1,17 @@
+const options = {
+  bottom: '64px', 
+  right: '32px', 
+  left: 'unset', 
+  time: '0.5s', 
+  mixColor: '#fff', 
+  backgroundColor: '#fff',  
+  buttonColorDark: '#100f2c',  
+  buttonColorLight: '#fff', 
+  saveInCookies: false, 
+  label: '🌓', 
+  autoMatchOsTheme: true 
+};
+
+const darkmode = new Darkmode(options);
+
+darkmode.showWidget();
